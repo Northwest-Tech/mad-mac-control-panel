e36cf1f9a9f864b126543674f9c54e7c94888f23
Jeremy <jeremy.skrdlant@nwktc.edu>
2025-04-16 07:12:24 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// http://10.200.6.42:42000/spot1/spot2/time
+// spot1 can be forwards / backward / or none 
+// spot2 can be left / right / or none 
+// time is in milliseconds. 2000 is 2 seconds. 
+
+//build your function here. 
